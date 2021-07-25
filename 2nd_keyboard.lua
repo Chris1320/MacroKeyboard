3e3eb06616505394b82e19166047d26d7318ca9f
Chris1320 <chris1320is@protonmail.com>
2021-07-25 11:52:50 +0800

    Finished 2nd_keyboard.lua
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- 2nd_keyboard.lua
+++ 2nd_keyboard.lua
@@ -1,8 +1,14 @@
 local string = require "string";
 local io = require "io";  -- For file operations
 
+-- minimizes the luamacros window
+lmc.minimizeToTray = true
+lmc_minimize()
+
 -- Change the path here if you want to use a different configuration file.
 local configfile = "./config.txt";
+
+print("Reading configuration file `" .. configfile .. "`...")
 
 function split_str(inputstr, sep) -- From `https://stackoverflow.com/a/7615129/15376542`
     if sep == nil then  -- If separator is nil then use whitespace.
@@ -38,4 +44,27 @@ end
 print("Keyboard ID: " .. kbID)
 print("Keyfile:     " .. keyfile)
 
-print("Waiting...")
+-- clear() -- clears the luamacros terminal
+
+lmc_device_set_name('MACROS',kbID); -- asigns a logical name to our secondary keyboard
+lmc_print_devices() -- prints all devices, you should se a device named MACROS
+
+print("\nWaiting for input...\n====================")
+
+-- function to write the key codes to <keyfile>.
+write_to_file = function (key)
+    print("Button: " .. key)
+    print("====================")
+    local file = io.open(keyfile, "w")
+    file:write(key)
+    file:flush() -- flush = save
+    file:close()
+    lmc_send_keys('{F24}')  -- Triggers the F24 key to tell autohotkey to read the key.txt file
+end
+
+-- define callback device MACROS 
+lmc_set_handler('MACROS' ,function(button, direction)
+    if (direction == 1) then return end  -- ignore down
+    write_to_file(button) -- executes function write_to_file with argument button (button = the ascii code of that key)
+    end
+)
